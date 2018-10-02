1987d3ec23c74a2f1f1025e15cf228ee41f361b7
Sher Chowdhury <sher.chowdhury@gmail.com>
2018-10-02 14:19:06 +0100

    wip

---
+++ files/python_basic_examples/01_basics/01_builtin_functions.py
@@ -0,0 +1,12 @@
+# There's a few built in functions:
+# https://docs.python.org/3.6/library/functions.html
+# The most common one is print:
+
+
+print('hello world')
+
+# another once is type, which prints what class an object belongs to:
+
+print(type('this is a string'))
+print(type(1234))
+
--- files/python_basic_examples/02_variables/01_strings/01_strings.py
+++ files/python_basic_examples/02_variables/01_strings/01_strings.py
@@ -1,2 +1,11 @@
 print('this is a string')
 print("you can also set strings using double quotes")
+
+multilinestring = '''this is a 
+multiline 
+string'''
+
+print(multilinestring)
+
+
+type(multilinestring)
